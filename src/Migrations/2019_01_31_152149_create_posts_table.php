e2c9bd48ed909819765e40b5d57cb0fbb7c34a6c
oliuz <oliuz@users.noreply.github.com>
2019-04-14 19:19:15 -0400

    Update 2019_01_31_152149_create_posts_table.php

--- src/Migrations/2019_01_31_152149_create_posts_table.php
+++ src/Migrations/2019_01_31_152149_create_posts_table.php
@@ -15,7 +15,7 @@ class CreatePostsTable extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->increments('id');
-            $table->integer("user_id")->unsigned();
+            $table->unsignedBigInteger('user_id');
             $table->foreign("user_id")->references("id")->on("users")->onDelete('cascade');
             $table->string("title")->nullable();
             $table->longText("content")->nullable();
