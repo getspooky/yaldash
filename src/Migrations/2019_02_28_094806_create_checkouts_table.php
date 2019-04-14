2cceac5c98cc213ca5357f868607bf8f149a1801
oliuz <oliuz@users.noreply.github.com>
2019-04-14 19:21:56 -0400

    Update 2019_02_28_094806_create_checkouts_table.php

--- src/Migrations/2019_02_28_094806_create_checkouts_table.php
+++ src/Migrations/2019_02_28_094806_create_checkouts_table.php
@@ -15,7 +15,7 @@ class CreateCheckoutsTable extends Migration
     {
         Schema::create('checkouts', function (Blueprint $table) {
             $table->increments('id');
-            $table->integer('user_id')->unsigned();
+            $table->unsignedBigInteger('user_id');
             $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
             $table->string('FirstName',30);
             $table->string('LastName',30);
