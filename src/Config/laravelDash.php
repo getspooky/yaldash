0c0303ce06d8931bbea3d3f6da06db251e36cb02
yasser <spookyframework@gmail.com>
2020-05-01 14:40:20 +0200

    refactor(config): Rename LaravelDash name

--- src/Config/laravelDash.php
+++ src/Config/laravelDash.php
@@ -1,8 +1,14 @@
 <?php
-
+/*
+ * This file is part of the laravelDash package.
+ *
+ * (c) Yasser Ameur El Idrissi <getspookydev@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
 
   return [
-
       /*
      |--------------------------------------------------------------------------
      | Application Name
@@ -13,8 +19,7 @@
      | any other location as required by the application or its packages.
      |
      */
-
-      'name' => 'Laravel Dashboard',
+      'name' => 'LaravelDash',
 
       /*
        |----------------------------------------------------------------------------
@@ -24,7 +29,6 @@
        | and Login page.By default, the logo is stored in the published / img folder.
        |
        */
-
       'logo' => 'logo-128.png',
 
       /*
@@ -37,7 +41,6 @@
       |
       |
       */
-
       'prefix' => '/',
 
       /*
@@ -48,7 +51,6 @@
       | Here you can customize the name of all routes provided by Laravel Dashboard
       |
       */
-
       'routes' => [
 
           'Dashboard', 'Settings', 'Manage', 'JsonManage' , 'Users' , 'Checkout' , 'Store'
@@ -63,12 +65,8 @@
       | Here you can add your own custom middleware.
       |
       */
-
-
       'middleware' => [
-
          'auth','web'
-
       ],
 
 
@@ -81,67 +79,41 @@
       | Modify just Dashboard view ,
       |
       */
-
       'views' => [
-
           'Post' => [
-
               'intro_post' => 'A place to publish your best work !',
-
               'desc_post' => 'A place where words matter. LaravelDash taps into the brains of the world\'s most insightful writers and thinkers.'
-
           ],
 
           'Store' => [
-
                'intro_store' => 'Easy Online Shopping‎ !',
-
                'desc_store' => 'Online shopping for the latest electronics, fashion, phone accessories, computer electronics, toys, home&garden, home appliances, tools, home improvement ...'
-
           ],
 
           'Dashboard' => [
-
               'intro_paragraph' => 'Build Laravel Web Applications faster than ever , Easy to install , Effortless to customize.',
-
               'intro_button' => 'Learn about what I do',
-
               'button_link' => '',
 
               'stuff' => [
-
                   'title' => "Here's all the stuff I do.",
-
                   'sub_title' => 'Revolutionize how you build the web.',
-
                   'first_stuff' => [
-
                        'icon' => 'icon featured fa fa-diamond',
-
                        'title' => 'Modern UI',
-
                        'content' => 'Laravel Dashboard provides a modern, responsive interface that you can be proud to put in front of your users.'
-
                   ],
 
                   'second_stuff' => [
-
                       'icon' => 'icon featured fa-flash',
-
                       'title' => 'Ridiculously fast',
-
                       'content' => 'Laravel Dashboard was designed to help developers take applications from concept to completion as quickly as possible.'
-
                   ],
 
                   'third_stuff' => [
-
                       'icon' => 'icon featured fa fa-book',
-
                       'title' => 'Easy to work with',
-
                       'content' => 'We have provided detailed documentation as well as specific code examples to help new users get started.'
-
                   ]
 
               ]
