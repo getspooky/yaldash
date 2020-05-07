7c6c1b367db8dc40a69a4b068c39e72a8ddabe00
yasser <spookyframework@gmail.com>
2020-05-08 00:54:03 +0200

    refactor(Models): Remove unused packages

--- src/Models/Buy.php
+++ src/Models/Buy.php
@@ -1,4 +1,12 @@
 <?php
+/*
+ * This file is part of the laravelDash package.
+ *
+ * (c) Yasser Ameur El Idrissi <getspookydev@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
 
 namespace Yasser\LaravelDashboard\Models;
 
@@ -6,7 +14,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Buy extends Model
 {
-    //
+
     protected $guarded = ['id'];
 
     protected $fillable = ['store_id','user_id'];
@@ -15,7 +23,6 @@ class Buy extends Model
      *
      * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
      */
-
     public function store()
     {
         return $this->belongsTo(Store::class);
