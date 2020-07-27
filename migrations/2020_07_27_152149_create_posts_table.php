392dcfc851f235995ab997748c203faf0ced9c87
yasser <spookyframework@gmail.com>
2020-07-27 22:32:34 +0200

    refactor(migrations): Rename 2020_07_27_152149_create_posts_table.php

--- migrations/2020_07_27_152149_create_posts_table.php
+++ migrations/2020_07_27_152149_create_posts_table.php
@@ -1,4 +1,12 @@
 <?php
+/*
+ * This file is part of the laravelDash package.
+ *
+ * (c) Yasser Ameur El Idrissi <getspookydev@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
 
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
