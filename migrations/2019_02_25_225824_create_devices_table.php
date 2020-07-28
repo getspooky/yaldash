77b5e1e19f649441296fc59ab2f07c74f74b9ba4
yasser <spookyframework@gmail.com>
2020-07-29 00:34:38 +0200

    refactor(migrations): Move create_devices_table  from src/Migrations to migrations

--- migrations/2019_02_25_225824_create_devices_table.php
+++ migrations/2019_02_25_225824_create_devices_table.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of the laravelDash package.
+ *
+ * (c) Yasser Ameur El Idrissi <getspookydev@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
@@ -11,7 +20,6 @@ class CreateDevicesTable extends Migration
      *
      * @return void
      */
-
     public function up()
     {
         Schema::create('devices', function (Blueprint $table) {
