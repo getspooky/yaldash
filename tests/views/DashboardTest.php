332af73a60efad235bce5f0f3af50d45288190bb
yasser <spookyframework@gmail.com>
2020-07-29 12:56:10 +0200

    fix(tests/views): Cannot find current namespace

--- tests/views/DashboardTest.php
+++ tests/views/DashboardTest.php
@@ -8,7 +8,7 @@
  * file that was distributed with this source code.
  */
 
-namespace YLaravelDash\Tests\Views;
+namespace LaravelDash\Tests\Views;
 
 use Illuminate\Support\Facades\Auth;
 use LaravelDashboard\Kit\BrowserKitTesting;
