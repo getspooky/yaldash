1a925c3dbdb07d423857b5772f66c2584244f068
yasser <spookyframework@gmail.com>
2020-07-29 13:03:30 +0200

    refactor(tests): Move StoreTest.php from src/Tests/views to tests/views

--- tests/views/StoreTest.php
+++ tests/views/StoreTest.php
@@ -1,17 +1,19 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: yasser
- * Date: 24/03/19
- * Time: 14:17
+/*
+ * This file is part of the laravelDash package.
+ *
+ * (c) Yasser Ameur El Idrissi <getspookydev@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
  */
 
-namespace Yasser\LaravelDashboard\Tests\Views;
+namespace LaravelDash\Tests\Views;
 
 use Illuminate\Foundation\Testing\DatabaseTransactions;
 use Illuminate\Http\File;
 use Illuminate\Support\Facades\Auth;
-use Yasser\LaravelDashboard\Kit\BrowserKitTesting;
+use LaravelDashboard\Kit\BrowserKitTesting;
 
 class StoreTest extends BrowserKitTesting
 {
@@ -20,28 +22,20 @@ class StoreTest extends BrowserKitTesting
     public function testStorePageRender()
     {
         $this->withoutMiddleware();
-
         $this->withExceptionHandling();
-
         $visit = $this->get(route('dashboard.store.index'));
-
         $visit->see('Price');
-
         $visit->see("Product");
     }
-
 
     public function testPostStore()
     {
         $this->withExceptionHandling();
-
-        $user = Auth::loginUsingId(1);
-
+        Auth::loginUsingId(1);
         $visit = $this->visit(route('dashboard.store.store'));
-
         $visit->type('Iphone 10', 'title')
             ->type('1032', 'price')
-            ->type(new File(dirname(__DIR__).'/File/logo.png'), 'file')
+            ->type(new File(dirname(__DIR__) . '/File/logo.png'), 'file')
             ->press('Product');
     }
 }
