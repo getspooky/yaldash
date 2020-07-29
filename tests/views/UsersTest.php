41c846f4fca0acacf419683181164ba124fd9239
yasser <spookyframework@gmail.com>
2020-07-29 13:08:00 +0200

    refactor(tests): Move UserTest.php from src/Tests/views to tests/views

--- tests/views/UsersTest.php
+++ tests/views/UsersTest.php
@@ -1,28 +1,26 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: yasser
- * Date: 25/03/19
- * Time: 17:45
+/*
+ * This file is part of the laravelDash package.
+ *
+ * (c) Yasser Ameur El Idrissi <getspookydev@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
  */
 
-namespace Yasser\Tests\Views;
+namespace LaravelDash\Tests\Views;
 
 use Illuminate\Support\Facades\Auth;
-use Yasser\LaravelDashboard\Kit\BrowserKitTesting;
+use LaravelDashboard\Kit\BrowserKitTesting;
 
 class UsersTest extends BrowserKitTesting
 {
     public function testUsersPageRender()
     {
         $this->withoutMiddleware();
-
         $this->withExceptionHandling();
-
         $visit = $this->get(route('dashboard.users'));
-
         $visit->see('Mrs. Kaylee Sauer Sr.');
-
         $visit->see('Active User ');
     }
 }
