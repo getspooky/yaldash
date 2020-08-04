c306e449fff3b48037c0e0f330c6695949f5f455
yasser <spookyframework@gmail.com>
2020-08-05 00:18:15 +0200

    feat(lib/providers): Add RouteServiceProvder class

---
+++ lib/Providers/RouteServiceProvider.php
@@ -0,0 +1,50 @@
+<?php
+/*
+ * This file is part of the laravelDash package.
+ *
+ * (c) Yasser Ameur El Idrissi <getspookydev@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace LaravelDashboard\Providers;
+
+use Illuminate\Foundation\Support\Providers\RouteServiceProvider as ServiceProvider;
+
+class RouteServiceProvider extends ServiceProvider {
+
+  /**
+   * This namespace is applied to your controller routes.
+   *
+   * In addition, it is set as the URL generator's root namespace.
+   *
+   * @var string
+   */
+  protected $namespace = 'LaravelDash\Controllers';
+
+  /**
+   * Define your route model bindings, pattern filters, etc.
+   *
+   * @return void
+   */
+   public function boot()
+   {
+    parent::boot();
+   }
+
+  /**
+   * Define the "web" routes for the application.
+   *
+   * These routes all receive session state, CSRF protection, etc.
+   *
+   * @return void
+   */
+  protected function mapWebRoutes()
+  {
+    Route::middleware('web')
+      ->namespace($this->namespace)
+      ->group(base_path('routes/web.php'));
+  }
+
+}
