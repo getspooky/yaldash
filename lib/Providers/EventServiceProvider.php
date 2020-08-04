37e62a82a43c8ea8cbfec23e28eff43bdfc298af
yasser <spookyframework@gmail.com>
2020-08-05 00:17:35 +0200

    refactor(lib/providers): Add License information

--- lib/Providers/EventServiceProvider.php
+++ lib/Providers/EventServiceProvider.php
@@ -1,5 +1,12 @@
 <?php
-
+/*
+ * This file is part of the laravelDash package.
+ *
+ * (c) Yasser Ameur El Idrissi <getspookydev@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
 
 namespace LaravelDashboard\Providers;
 
@@ -19,7 +26,6 @@ class EventServiceProvider extends ServiceProvider
         NotificationEvent::class=>[
             NotificationListeners::class
         ],
-
     ];
 
     /**
