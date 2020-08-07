935020aada6b3908fabfa37057c0ac7254a2dafa
yasser <spookyframework@gmail.com>
2020-08-08 01:06:17 +0200

    fix(lib/Traits/Assets): Cannot find endWith function

--- lib/Traits/Assets.php
+++ lib/Traits/Assets.php
@@ -25,17 +25,19 @@ trait Assets
     public function Dashboard_assets($folder, $file)
     {
         $path = str_replace(['../', './'], '', $file);
-        $path = dirname(__DIR__)."/../published/$folder/$path";
+        $path = __DIR__. "/../../published/$folder/$path";
         if (File::exists($path)) {
             $ContentType = null;
-            if (ends_with($path, '.js')) {
+            if (str_ends_with($path, '.js')) {
                 $ContentType = 'text/javascript';
-            } elseif (ends_with($path, '.css')) {
+            } elseif (str_ends_with($path, '.css')) {
                 $ContentType = 'text/css';
             } else {
                 $ContentType = File::mimeType($path);
             }
-            return response(File::get($path), 200, [ 'Content-Type' => $ContentType ]);
+            return response(File::get($path), 200,
+              [ 'Content-Type' => $ContentType ]
+            );
         }
     }
 }
