6a03570fc91b0153267b71a22fbaa439d416ea98
yasser <spookyframework@gmail.com>
2020-08-10 19:08:50 +0200

    feat(layouts/footers): Add nav blade template

---
+++ resources/views/layouts/footers/nav.blade.php
@@ -0,0 +1,26 @@
+<div class="row align-items-center justify-content-xl-between">
+  <div class="col-xl-6">
+    <div class="copyright text-center text-xl-left text-muted">
+      &copy; {{ now()->year }} <a href="https://github.com/getspooky" class="font-weight-bold ml-1" target="_blank">getspooky</a> &amp;
+    </div>
+  </div>
+  <div class="col-xl-6">
+    <ul class="nav nav-footer justify-content-center justify-content-xl-end">
+      <li class="nav-item">
+        <a href="https://medium.com/@yasserameur" class="nav-link" target="_blank">Medium</a>
+      </li>
+      <li class="nav-item">
+        <a href="https://dev.to/getspooky" class="nav-link" target="_blank">Dev.to</a>
+      </li>
+      <li class="nav-item">
+        <a href="" class="nav-link" target="_blank">About Us</a>
+      </li>
+      <li class="nav-item">
+        <a href="" class="nav-link" target="_blank">Blog</a>
+      </li>
+      <li class="nav-item">
+        <a href="https://github.com/getspooky/laravelDash/blob/master/LICENSE" class="nav-link" target="_blank">MIT License</a>
+      </li>
+    </ul>
+  </div>
+</div>
