0de08f28c796af0249d9569c870da96a5881060f
yasser <spookyframework@gmail.com>
2020-08-12 18:00:23 +0200

    fix: Remove resources/nucleo.css file

--- resources/views/layouts/navbars/navbar.blade.php
+++ resources/views/layouts/navbars/navbar.blade.php
@@ -1,7 +1,3 @@
 @auth()
   @include('LaravelDashboard::layouts.navbars.navs.auth')
 @endauth
-
-@guest()
-  @include('LaravelDashboard::layouts.navbars.navs.guest')
-@endguest
--- resources/views/layouts/navbars/navs/guest.blade.php
+++ resources/views/layouts/navbars/navs/guest.blade.php
@@ -1,8 +1,7 @@
-
 <nav class="navbar navbar-top navbar-horizontal navbar-expand-md navbar-dark">
   <div class="container px-4">
     <a class="navbar-brand" href="{{ route('home') }}">
-      <img src="/img/brand/white.png" />
+      <img src="https://argon-dashboard-laravel.creative-tim.com/argon/img/brand/white.png" />
     </a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar-collapse-main" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
@@ -13,7 +12,7 @@
         <div class="row">
           <div class="col-6 collapse-brand">
             <a href="{{ route('home') }}">
-              <img src="/img/brand/blue.png">
+              <img src="https://argon-dashboard-laravel.creative-tim.com/argon/img/brand/white.png">
             </a>
           </div>
           <div class="col-6 collapse-close">
@@ -28,25 +27,25 @@
       <ul class="navbar-nav ml-auto">
         <li class="nav-item">
           <a class="nav-link nav-link-icon" href="{{ route('home') }}">
-            <i class="ni ni-planet"></i>
+            <i class="fa fa-tachometer"></i>
             <span class="nav-link-inner--text">{{ __('Dashboard') }}</span>
           </a>
         </li>
         <li class="nav-item">
           <a class="nav-link nav-link-icon" href="{{ route('register') }}">
-            <i class="ni ni-circle-08"></i>
+            <i class="fa fa-user-plus"></i>
             <span class="nav-link-inner--text">{{ __('Register') }}</span>
           </a>
         </li>
         <li class="nav-item">
           <a class="nav-link nav-link-icon" href="{{ route('login') }}">
-            <i class="ni ni-key-25"></i>
+            <i class="fa fa-unlock-alt"></i>
             <span class="nav-link-inner--text">{{ __('Login') }}</span>
           </a>
         </li>
         <li class="nav-item">
           <a class="nav-link nav-link-icon" href="{{ route('profile.edit') }}">
-            <i class="ni ni-single-02"></i>
+            <i class="fa fa-user-circle"></i>
             <span class="nav-link-inner--text">{{ __('Profile') }}</span>
           </a>
         </li>
