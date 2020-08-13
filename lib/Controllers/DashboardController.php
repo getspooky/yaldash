d3305c33e473f9c3a3fc7f9bb5777eb53f20bc3c
yasser <spookyframework@gmail.com>
2020-08-14 01:24:33 +0200

    refactor(lib/Controllers/DashboardController): Rename LaravelDashboardController to DashboardController

--- lib/Controllers/DashboardController.php
+++ lib/Controllers/DashboardController.php
@@ -8,34 +8,22 @@
  * file that was distributed with this source code.
  */
 
-namespace LaravelDashboard\Controllers;
+namespace yal\laraveldash\Controllers;
 
-use App\User;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
-use LaravelDashboard\Mail\EmailSupport;
-use LaravelDashboard\Traits\Assets;
+use yal\laraveldash\Mail\EmailSupport;
+use yal\laraveldash\Traits\Assets;
 
-class LaravelDashboardController extends Controller
+class DashboardController extends Controller
 {
     use Assets;
 
-    /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
     public function __construct()
     {
         $this->middleware(['web', 'auth'])->except(['Dashboard_assets']);
     }
 
-
-    /**
-     * Show the application dashboard.
-     *
-     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\View\View
-     */
     public function index()
     {
         $user = auth()->user();
@@ -46,24 +34,14 @@ class LaravelDashboardController extends Controller
              "EARNINGS" => $earning,
              "POINTS"   => $points
          ];
-        return view("LaravelDashboard::index", $state);
+        return view("LaravelDashboard::dashboard", $state);
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  Request  $request
-     * @return mixed
-     */
     public function store_draft(Request $request)
     {
         return redirect()->route('dashboard.home');
     }
 
-    /**
-     * Get the views state
-     * @return \Illuminate\Http\JsonResponse
-     */
     public function ViewsState()
     {
         $user = auth()->user()->devices();
