c58435ccea86b94659eff99f521682169797a11b
yasser <spookyframework@gmail.com>
2020-08-14 14:54:27 +0200

    refactor(routes): Remove Laravel prefix from controllers

--- routes/web.php
+++ routes/web.php
@@ -8,52 +8,53 @@
  * file that was distributed with this source code.
  */
 
-$namespacePrefix = 'LaravelDashboard\Controllers';
 
-Route::prefix('demo')->group(function () use ($namespacePrefix) {
+$namespacePrefix = 'yal\laraveldash\Controllers';
+
+Route::prefix(config('laravelDash.prefix'))->group(function () use ($namespacePrefix) {
 
   Route::get('/', function () {
-    return view('LaravelDashboard::welcome');
+    return view('\yal\laraveldash::welcome');
   });
 
-  Route::get('Dashboard', $namespacePrefix. '\LaravelDashboardController@index')
+  Route::get('Dashboard', $namespacePrefix. '\DashboardController@index')
     ->name('dashboard.home');
-  Route::resource('post', $namespacePrefix. '\LaravelPostController');
-  Route::get('Settings',  $namespacePrefix. '\LaravelSettingsController@index')
+  Route::resource('post', $namespacePrefix. '\PostController');
+  Route::get('Settings',  $namespacePrefix. '\SettingsController@index')
             ->name('dashboard.settings.index');
-  Route::put('Settings',  $namespacePrefix .'\LaravelSettingsController@Update')
+  Route::put('Settings',  $namespacePrefix .'\SettingsController@Update')
             ->name('dashboard.settings.update');
-  Route::post('Settings/upload/avatar', $namespacePrefix. '\LaravelSettingsController@Upload')
+  Route::post('Settings/upload/avatar', $namespacePrefix. '\SettingsController@Upload')
             ->name('dashboard.settings.upload_avatar');
-  Route::post('Settings/delete', $namespacePrefix. '\LaravelSettingsController@Delete')
+  Route::post('Settings/delete', $namespacePrefix. '\SettingsController@Delete')
             ->name('dashboard.settings.delete_account.destroy');
-  Route::get('Manage', $namespacePrefix. '\LaravelManageController@index')
+  Route::get('Manage', $namespacePrefix. '\Controller@index')
             ->name('dashboard.manage.index');
-  Route::get('JsonManage', $namespacePrefix. '\LaravelManageController@Response')
+  Route::get('JsonManage', $namespacePrefix. '\ManageController@Response')
             ->name('dashboard.manage.jsonData');
-  Route::get('Users', $namespacePrefix. '\LaravelSubscribeController@index')
+  Route::get('Users', $namespacePrefix. '\SubscribeController@index')
             ->name('dashboard.users');
-  Route::post('Users', $namespacePrefix. '\LaravelSubscribeController@store')
+  Route::post('Users', $namespacePrefix. '\SubscribeController@store')
             ->name('dashboard.users.store');
-  Route::put('Manage/{id}/{type}', $namespacePrefix. '\LaravelManageController@Delete')
+  Route::put('Manage/{id}/{type}', $namespacePrefix. '\ManageController@Delete')
             ->name('dashboard.manage.delete');
-  Route::get('Checkout', $namespacePrefix. '\LaravelCheckoutController@index')
+  Route::get('Checkout', $namespacePrefix. '\DashboardCheckoutController@index')
             ->name('dashboard.checkout.index');
-  Route::post('Checkout', $namespacePrefix. '\LaravelCheckoutController@charges')
+  Route::post('Checkout', $namespacePrefix. '\DashboardCheckoutController@charges')
             ->name('dashboard.checkout.charges');
-  Route::get('Store', $namespacePrefix. '\LaravelStoreController@index')
+  Route::get('Store', $namespacePrefix. '\StoreController@index')
             ->name('dashboard.store.index');
-  Route::post('Store', $namespacePrefix. '\LaravelStoreController@store')
+  Route::post('Store', $namespacePrefix. '\StoreController@store')
             ->name('dashboard.store.store');
-  Route::get('Sell', $namespacePrefix. '\LaravelSellController@index')
+  Route::get('Sell', $namespacePrefix. '\SellController@index')
             ->name('dashboard.sell.index');
-  Route::post('Buy/{id}', $namespacePrefix. '\LaravelStoreController@buy')
+  Route::post('Buy/{id}', $namespacePrefix. '\StoreController@buy')
             ->name('dashboard.store.buy');
-  Route::post('View/device/{id}', $namespacePrefix. '\LaravelPostController@DevicesStore')
+  Route::post('View/device/{id}', $namespacePrefix. '\PostController@DevicesStore')
             ->name('dashboard.post.DevicesStore');
-  Route::get('published/{folder}/{file}', $namespacePrefix. '\LaravelDashboardController@Dashboard_assets')
+  Route::get('published/{folder}/{file}', $namespacePrefix. '\DashboardController@Dashboard_assets')
             ->name('dashboard.assets');
-  Route::get('ViewsState', $namespacePrefix. '\LaravelDashboardController@ViewsState')
+  Route::get('ViewsState', $namespacePrefix. '\DashboardController@ViewsState')
             ->name('dashboard.views.state');
 
 });
