f3a70bcf9a97855fb3eab412015c419a33aea9d8
yasser <spookyframework@gmail.com>
2020-08-14 02:17:37 +0200

    fix(lib/Traits/Assets): Format & Lint Assets

--- lib/Traits/Assets.php
+++ lib/Traits/Assets.php
@@ -8,24 +8,19 @@
  * file that was distributed with this source code.
  */
 
-namespace LaravelDashboard\Traits;
+namespace yal\laraveldash\Traits;
 
 use Illuminate\Http\Response;
 use Illuminate\Support\Facades\File;
 
 trait Assets
 {
-    /**
-     * The Dashboard_assets function generates a URL for an asset using the current scheme of the request
-     *
-     * @param $folder
-     * @param $file
-     * @return Response
-     */
+
     public function Dashboard_assets($folder, $file)
     {
         $path = str_replace(['../', './'], '', $file);
         $path = __DIR__. "/../../published/$folder/$path";
+
         if (File::exists($path)) {
             $ContentType = null;
             if (str_ends_with($path, '.js')) {
@@ -35,9 +30,10 @@ trait Assets
             } else {
                 $ContentType = File::mimeType($path);
             }
-            return response(File::get($path), 200,
-              [ 'Content-Type' => $ContentType ]
-            );
+
+            return response(File::get($path), 200, [ 'Content-Type' => $ContentType ]);
+
         }
+
     }
 }
