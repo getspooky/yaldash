086abdd9719200fb81aa3ead5864bac4095dad8e
Mouad ZIANI <mouad.ziani1997@gmail.com>
2021-11-03 11:51:56 +0100

    fix: little html syntax error

--- lib/Stubs/template.blade.php
+++ lib/Stubs/template.blade.php
@@ -1,14 +1,12 @@
 @extends("yal\laraveldash::home")
 
 @section("content")
-
   @component("yal\laraveldash::components.navbar")
     <div class="page-header row no-gutters py-4">
       <div class="col-12 col-sm-4 text-center text-sm-left mb-0">
         <span class="text-uppercase page-subtitle">Dashboard</span>
         <h3 class="page-title" style="margin-top:10px;">Template Overview</h3>
       </div>
-    @endcomponent
     </div>
-
+  @endcomponent
 @endsection
